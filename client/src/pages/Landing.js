00007b85d7217746bd5ab894a72641b1419d3a3e
Denim Hodgson <denimhodgson@gmail.com>
2023-02-01 13:49:53 +0800

    Landing page complete

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,29 +1,69 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 2rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt='jobHive' className='logo' />
-        <div class='container page'>
-          <div className='info'>
-            <h1>
-              jobHive <span>Tracking</span> App
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
-              asperiores dolor, cumque voluptas quis pariatur libero corporis!
-              Aut architecto, porro libero nisi sequi voluptate vitae cupiditate
-              exercitationem illum voluptates quod, reiciendis ipsum iusto quos
-              veniam officiis totam quia alias ea?
-            </p>
-            <button className='btn btn-hero'>Login/Register</button>
-          </div>
-          <img src={main} alt='job hunt' className='image'></img>
-        </div>
       </nav>
-    </main>
+      <div class='container page'>
+        <div className='info'>
+          <h1>
+            jobHive <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+            asperiores dolor, cumque voluptas quis pariatur libero corporis! Aut
+            architecto, porro libero nisi sequi voluptate vitae cupiditate
+            exercitationem illum voluptates quod, reiciendis ipsum iusto quos
+            veniam officiis totam quia alias ea?
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img'></img>
+      </div>
+    </Wrapper>
   );
 };
 export default Landing;
