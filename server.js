62b58b643fd422e80e1aaebec74b52e4283587fb
Denim Hodgson <denimhodgson@gmail.com>
2023-02-07 11:05:17 +0800

    Express JS setup, server route added
    Simple home route added to test server functionality and express working

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Server is running...');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Server!');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
