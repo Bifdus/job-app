887ed45eb6807d89d04421f3124b7c2ae79d3991
Denim Hodgson <denimhodgson@gmail.com>
2023-02-14 11:45:00 +0800

    Mongo DB connection
    + mongoose install

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
-
 const app = express();
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import connectDB from './db/connect.js';
 
 // Must have .js extension
 import notFoundMiddleWare from './middleware/not-found.js';
@@ -17,6 +21,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
