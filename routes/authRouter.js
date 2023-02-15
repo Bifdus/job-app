a10989f7d16c0f1e23bca908eb456fdc110b295f
Denim Hodgson <denimhodgson@gmail.com>
2023-02-15 23:04:08 +0800

    Fixing bug found in route
    Postman testing threw an error, missing / in the route string

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,6 +4,6 @@ import { register, login, updateUser } from '../controllers/authController.js';
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('updateUser').patch(updateUser);
+router.route('/updateUser').patch(updateUser);
 
 export default router;
