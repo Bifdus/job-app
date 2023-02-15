86268fea1dccfba9b12d2e89c55f56707d45c251
Denim Hodgson <denimhodgson@gmail.com>
2023-02-15 23:19:10 +0800

    User Schema Creation

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'Perth, WA',
+  },
+  role: {
+    type: String,
+    trim: true,
+    maxlength: 50,
+    default: 'Graduate Developer',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
