a4bf61c2f821e968d320abd5b99438de87c3b0e1
Denim Hodgson <denimhodgson@gmail.com>
2023-02-16 10:44:58 +0800

    Basic error handling implementation
    error is handled by the middleware using next

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,12 @@
-const register = (req, res) => {
+import User from '../models/User.js';
+
+const register = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
   res.send('register user');
 };
 
