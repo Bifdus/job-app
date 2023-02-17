efbd1d4d64936f18d7725699274995c14a03e571
Denim Hodgson <denimhodgson@gmail.com>
2023-02-17 13:39:36 +0800

    Proxy initial setup

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Server!');
+  res.json({ msg: 'Welcome to the JobHive API' });
+});
+
+app.get('/api/v1', (req, res) => {
+  res.json({ msg: 'API' });
 });
 
 app.use('/api/v1/auth', authRouter);
